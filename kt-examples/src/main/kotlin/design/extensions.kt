341f822e447b4c5ffade6bd45711d450a748eb71
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2023-06-03 00:56:10 +0300

    Final slides

---
+++ kt-examples/src/main/kotlin/design/extensions.kt
@@ -0,0 +1,18 @@
+interface A {
+    fun foo()
+}
+
+class B : A {
+    override fun foo() {
+        print("Hello")
+    }
+}
+
+fun <T : A> T.extension(): T = this
+
+fun main() {
+    B()
+        .extension()
+        .extension()
+        .foo()
+}
